ac0facab9ec9bdadc51d067cc6f29727578443d0
oscaresantosg <32517366+oscaresantosg@users.noreply.github.com>
2022-02-14 18:13:25 -0500

    Add files via upload
    Add files to spring initializer   for  Springboot project  call "PACMANWEB"

---
+++ JavaProyect/src/main/java/co/edu/uptc/JavaProyect/PacmanWebApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.JavaProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacmanWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacmanWebApplication.class, args);
+	}
+
+}
---
+++ JavaProyect/src/test/java/co/edu/uptc/JavaProyect/PacmanWebApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.JavaProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacmanWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
